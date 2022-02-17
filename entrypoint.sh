0d342b7faeac172eccd6e78474336c9dd2b34b82
Sammy Sherman <sammytsherman@gmail.com>
2022-02-16 22:22:02 -0600

    Update entrypoint to set permissions correctly

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -n "$UID" && "$UID" -ne "0" ]]; then
-  echo "Using UID of $UID"
-  chown -R $UID /rlcraft
-  adduser -u $UID rlcraftuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-else
-  echo "Adding rlcraft user"
-  adduser rlcraftuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+# Get the current user based on the UID value
+USER=$(cat /etc/passwd | grep :$UID: | cut -d ':' -f1)
+
+# If there is no user with the UID, add one with the UID and change ownership of /rlcraft
+if [[ -z "$USER" ]]; then
+    adduser -u $UID --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password rlcraftuser
+    chown -R $UID /rlcraft
+    USER=rlcraftuser
 fi
+# If there is a GID passed, add a new group with the GID and change the current users primary group to it then change ownership of /rlcraft and give groups same permissions as user for /rlcraft
 if [[ -n "$GID" ]]; then
-  groupadd rlcraftgroup -g $GID
-  chown -R :$GID /rlcraft
-  chmod -R g=u /rlcraft
-  usermod -a -G rlcraftgroup rlcraftuser
+    groupadd rlcraftgroup -g $GID
+    usermod -g rlcraftgroup $USER
+    chown -R $UID:$GID /rlcraft
+    chmod -R g=u /rlcraft
+    umask 002
 fi
-
-su rlcraftuser <<SHT
-     cd /rlcraft
-     java -jar forge-1.12.2-14.23.5.2860.jar
+# Run the jar as the correct user
+su $USER <<SHT
+    cd /rlcraft
+    java -jar forge-1.12.2-14.23.5.2860.jar
 SHT
