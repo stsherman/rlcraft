9da67934a54af70d53740f277ea174d5e677f9af
Sammy Sherman <sammmytsherman@gmail.com>
2022-02-23 23:48:38 -0600

    Add backup.sh with outline for how it will need to be filled out

---
+++ backup.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# We want to back up:
+## World
+## banned-ips.json
+## banned-players.json
+## ops.json
+## server.properties
+## whitelist.json
+### Essentially *.json
+
+
+# Script should allow backing up and restoring from backups
+
+# If backing up
+## Pause docker container or stop server using server commands (still need to figure out how to do this programmatically)
+## Tarball all the files/directories listed above named with the date and time
+## Move Tarball to a backup folder
+## Resume docker container or server
+
+# If restoring a backup
+## Verify that the file given exists
+## Verify that the working directory has the other necessary files to run the server (not sure if this is necessary)
+## Pause docker container or stop server using server commands (still need to figure out how to do this programmatically)
+## Extract tarball and overwrite files/directories
+## Resume docker container or server
+
+
+# TODO
+## Figure out how to avoid issues with the session.lock
+## Figure out issues with sending server commands programmatically
